7d628940a0d5121e82d96003b5e3f53c77be148b
P1xByte <109861687+P1xByte@users.noreply.github.com>
2022-11-07 17:31:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_ready() -> None:
     print(f"{bot.user} ин да хаус!.")
     await bot.change_presence(activity=Activity(name="музыку",  type=ActivityType.listening))
 
-    voice_channel = bot.get_channel(884421383940087808)
+    voice_channel = bot.get_channel(1038420981561184286)
     player = await voice_channel.connect()
     player.play(FFmpegPCMAudio("http://89.185.94.89:8000/rd_72_01"))
     
